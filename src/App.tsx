b2d9f0c3ee9a88d07f4b6cb4236cff810e79c964
Anubhav Pathak <anubhavpathak99@gmail.com>
2022-10-04 16:57:05 +0530

    Fixed Route Pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./Pages/ErrorPage";
 import HomePage from "./Pages/HomePage/HomePage";
 import RegisterPage from "./Pages/RegisterPage/RegisterPage";
-//import WelcomePage from "./Pages/WelcomePage/WelcomePage";
-//import TaskPage from "./Pages/TaskPage/TaskPage";
+import WelcomePage from "./Pages/WelcomePage/WelcomePage";
+import TaskPage from "./Pages/TaskPage/TaskPage";
 
 const router = createBrowserRouter([
   {
@@ -19,8 +19,18 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage />,
+    errorElement:  <ErrorPage />
+  },
+  {
+    path: "/welcome",
+    element: <WelcomePage />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "/task",
+    element: <TaskPage />,
+    errorElement: <ErrorPage />,
+  }
 ]);
 
 function App() {
--- src/Pages/WelcomePage/WelcomePage.jsx
+++ src/Pages/WelcomePage/WelcomePage.jsx
@@ -4,28 +4,30 @@ import "./WelcomePage.css";
 function WelcomePage(){
     const [subDomain, setSubDomain] = React.useState('');
     return (
-        <main>
-            <section>
-                <h1> Welcome, User ABC </h1>
-                <p> You have selected Tech/// please select your subdomain </p>
-                <form action="#">
-                    <input type="hidden" name="subDomain" id="sub-domain" value={subDomain} />
-                    <div className="clip_2">
-                        <input type="submit" value="AI/ML" onClick={()=>{setSubDomain("AI/ML")}} />
-                    </div>
-                    <div className="clip_2">
-                        <input type="submit" value="Blockchain" onClick={()=>{setSubDomain("Blockchain")}} />
-                    </div>
-                    <div className="clip_2">
-                        <input type="submit" value="Web Dev" onclick={()=>{setSubDomain("Web Development")}} />
-                    </div>
-                </form>
-                <p className="note"> *you can't change your sub-domain after selecting </p>
-            </section>
-            <aside className="img-container">
-                <img src={logo} alt="Data Science" />
-            </aside>
-        </main>
+        <div className="welcome">
+            <main className="welcome">
+                <section className="welcome">
+                    <h1> Welcome, User ABC </h1>
+                    <p> You have selected Tech/// please select your subdomain </p>
+                    <form action="/task" className="welcome" method="GET">
+                        <input type="hidden" name="subDomain" id="sub-domain" value={subDomain} />
+                        <div className="welcome-clip_2">
+                            <input type="submit" value="AI/ML" onClick={()=>{setSubDomain("AI/ML")}} />
+                        </div>
+                        <div className="welcome-clip_2">
+                            <input type="submit" value="Blockchain" onClick={()=>{setSubDomain("Blockchain")}} />
+                        </div>
+                        <div className="welcome-clip_2">
+                            <input type="submit" value="Web Dev" onclick={()=>{setSubDomain("Web Dev")}} />
+                        </div>
+                    </form>
+                    <p className="welcome-note"> *you can't change your sub-domain after selecting </p>
+                </section>
+                <aside className="img-container">
+                    <img src={logo} alt="Data Science" />
+                </aside>
+            </main>
+        </div>
     )
 }
 export default WelcomePage;
